37ea1bf300623e8120821591117c66b49bdc7909
Manoj K <Manoj.kavil@hotmail.com>
2018-11-28 19:33:42 +0530

    test15

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace WebApplication1.Controllers
     {
         public IActionResult Index()
         {
-            var a = 10 / 1;
+            var a = 10 / 10;
             return View();
         }
 
